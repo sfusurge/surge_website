73d44c3544fcb39c55158811df3cc1af0d15cce6
Joshua Peng <jpa95@sfu.ca>
2021-05-17 20:13:57 -0700

    Happy joshua will work on making it more responsive

--- src/components/Podcast.js
+++ src/components/Podcast.js
@@ -1,26 +1,26 @@
 import useMediaQuery from '../utils/useMediaQuery'
 import styles from '../css/Podcast.module.css'
+import josh from '../assets/podcastHosts/josh.png'
 import PodcastTitle from './PodcastTitle'
 
 const Podcast = props => {
   const isDesktop = useMediaQuery('(min-width: 600px)')
   return (
     <>
-      {isDesktop && (
-        <div className={styles.container}>
-          <div className={styles.text}>
-            <div className={styles.title}>
-              <PodcastTitle />
-            </div>
-            <p className={styles.subTitle}>Then vs Now: Surge's Dynamic Duo</p>
-            <p className={styles.hosts}>{'with Vivian Wong & Noble Tan'}</p>
-            <p className={styles.date}>Released on April 9, 2021 </p>
-            <a href="https://www.facebook.com/sfusurge/">
-              <div>Take me to the episode!</div>
-            </a>
+      <div className={styles.container}>
+        <img className={styles.person} src={josh}></img>
+        <div className={styles.text}>
+          <div className={styles.title}>
+            <PodcastTitle />
           </div>
+          <p className={styles.subTitle}>Then vs Now: Surge's Dynamic Duo</p>
+          <p className={styles.hosts}>{'with Vivian Wong & Noble Tan'}</p>
+          <p className={styles.date}>Released on April 9, 2021 </p>
+          <a>
+            <div className={styles.btn}>Facebook Event</div>
+          </a>
         </div>
-      )}
+      </div>
     </>
   )
 }
