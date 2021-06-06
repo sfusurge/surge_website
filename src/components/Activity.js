4a6c166bf4a622e3cc13fbfce1b6a5e4319d656c
Joshua Peng <jpa95@sfu.ca>
2021-06-06 15:45:29 -0700

    Make a tags open new tab

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -21,6 +21,8 @@ const Activity = props => {
               <a
                 className={styles.link}
                 href="https://open.spotify.com/show/5GAuw7brERfbZSfnKpURvE?si=09bf7b886d3a448b"
+                target="_blank"
+                rel="noreferrer"
               >
                 <div className={styles.verticalAlign}>
                   <img
@@ -35,6 +37,8 @@ const Activity = props => {
             <a
               className={styles.streamingLink}
               href="https://anchor.fm/sfu-surge"
+              target="_blank"
+              rel="noreferrer"
             >
               More Streaming Platforms
             </a>
