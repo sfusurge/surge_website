615421ea0d3259f3e0bf9b4b54664928076b0dbb
Joshua Peng <jpa95@sfu.ca>
2021-06-12 13:42:39 -0700

    Revert app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Footer from './components/Footer'
+
 import styles from './css/App.module.css'
 
 const App = () => {
@@ -7,22 +7,9 @@ const App = () => {
     <div id={styles.app}>
       <Router>
         <Switch>
-          <Route path="/path">
-            <div style={{ height: '1240px' }}>
-              <div>Block</div>
-              <div>Block</div>
-              <div>Block</div>
-              <div>Block</div>
-              <div>Block</div>
-              <div>Block</div>
-              <div>Block</div>
-              <div>Block</div>
-              <div>Block</div>
-              <div>Block</div>
-              <div>Block</div>
-            </div>
-            <Footer />
-          </Route>
+          {/* <Route path="/path">
+            <Component />
+          </Route> */}
         </Switch>
       </Router>
     </div>
