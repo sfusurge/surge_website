be6b32ca838165214ae6abf52d9072ccabe12625
Joshua Peng <jpa95@sfu.ca>
2021-08-01 17:12:37 -0700

    completed header components for Home, Events, Projects, etc.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import HomeBody from './components/HomeBody'
-import NavBar from './components/NavBar'
 
 import styles from './css/App.module.css'
 
@@ -9,9 +7,9 @@ const App = () => {
     <div id={styles.app}>
       <Router>
         <Switch>
-          <Route path="/path">
-            <HomeBody />
-          </Route>
+          {/* <Route path="/path">
+            <Component />
+          </Route> */}
         </Switch>
       </Router>
     </div>
--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -1,23 +1,76 @@
 import useMediaQuery from '../utils/useMediaQuery'
-import styles from '../css/HomeBody.module.css'
+import styles from '../css/HomeSection.module.css'
 import rocketMonitor from '../assets/rocket-monitor.svg'
+import rocketMonitorMobile from '../assets/rocket-monitor-mobile.svg'
 import sfuSurge from '../assets/sfu-surge.svg'
+import sfuSurgeMobile from '../assets/sfu-surgeMobile.svg'
+import { Link } from 'react-router-dom'
 
-const HomeBody = props => {
+import IntroBody from './IntroBody'
+import NavBar from './NavBar'
+
+const HomeSection = props => {
+  const isDesktop = useMediaQuery('(min-width: 600px)')
   return (
     <>
-      <img className={styles.logo} src={sfuSurge} alt={sfuSurge} />
-      <p className={styles.header}>Engage • Empower • Innovate</p>
-      <p className={styles.bodyText}>
-        <span className={styles.boldText}>{'SFU Surge '}</span>
-        is an all-inclusive organization to promote
-        <span className={styles.boldText}>
-          {' curiosity and excitement for a future in technology'}
-        </span>
-        , while guiding students on their paths to career success.
-      </p>
+      {isDesktop ? (
+        <IntroBody
+          imageSrc={rocketMonitor}
+          stylingTextGroup={{
+            marginTop: '13vh'
+          }}
+          scrollBar={true}
+        >
+          <img className={styles.logo} src={sfuSurge} alt={sfuSurge} />
+          <p className={styles.header}>Engage • Empower • Innovate</p>
+          <p
+            style={isDesktop ? { lineHeight: '32px' } : { lineHeight: '20px' }}
+            className={styles.bodyText}
+          >
+            <span className={styles.boldText}>{'SFU Surge '}</span>
+            is an all-inclusive organization to promote
+            <span className={styles.boldText}>
+              {' curiosity and excitement for a future in technology'}
+            </span>
+            , while guiding students on their paths to career success.
+          </p>
+          <Link to="/about" className={styles.link}>
+            <div className={styles.btn}>Learn More</div>
+          </Link>
+        </IntroBody>
+      ) : (
+        <div className={styles.mobileContainer}>
+          <div className={styles.navBarMobile}>
+            <NavBar />
+          </div>
+          <div className={styles.mobileText}>
+            <img
+              className={styles.mobileLogo}
+              src={sfuSurgeMobile}
+              alt={sfuSurgeMobile}
+            />
+            <p className={styles.header}>Engage • Empower • Innovate</p>
+            <p className={styles.bodyText}>
+              <span className={styles.boldTextMobile}>{'SFU Surge '}</span>
+              is an all-inclusive organization to promote
+              <span className={styles.boldTextMobile}>
+                {' curiosity and excitement for a future in technology'}
+              </span>
+              , while guiding students on their paths to career success.
+            </p>
+            <Link to="/about" className={styles.link}>
+              <div className={styles.btn}>Learn More</div>
+            </Link>
+          </div>
+          <img
+            src={rocketMonitorMobile}
+            alt={rocketMonitorMobile}
+            className={styles.rocketMonitorMobile}
+          />
+        </div>
+      )}
     </>
   )
 }
 
-export default HomeBody
+export default HomeSection
