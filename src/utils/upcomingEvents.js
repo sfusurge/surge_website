0f450d7e325e0abb4f76dca0639af8d1e56d0c5a
Joshua Peng <jpa95@sfu.ca>
2021-08-27 22:12:27 -0700

    added a min height to component

--- src/utils/upcomingEvents.js
+++ src/utils/upcomingEvents.js
@@ -10,6 +10,20 @@ const upcomingEvent = [
     filterSetting: 'Workshops'
   },
   {
+    primaryColor: 'var(--color-button-secondary)',
+    secondaryColor: '#447CCE',
+    title: 'Linux Workshop',
+    day: '22',
+    month: 'Nov',
+    location: 'SFU Surge Discord Server',
+    time: '7:00PM - 9:00PM',
+    link: 'https://mail.sfu.ca/owa/#path=/mail',
+    fbEvent: 'https://www.facebook.com/',
+    filterSetting: 'Workshops'
+  },
+  {
+    primaryColor: 'var(--color-image-background)',
+    secondaryColor: '#ADBED8',
     title: 'Linux Workshop',
     day: '22',
     month: 'Nov',
@@ -30,16 +44,8 @@ const upcomingEvent = [
     filterSetting: 'Workshops'
   },
   {
-    title: 'Linux Workshop',
-    day: '22',
-    month: 'Nov',
-    location: 'SFU Surge Discord Server',
-    time: '7:00PM - 9:00PM',
-    link: 'https://mail.sfu.ca/owa/#path=/mail',
-    fbEvent: 'https://www.facebook.com/',
-    filterSetting: 'Workshops'
-  },
-  {
+    primaryColor: 'var(--color-button-secondary)',
+    secondaryColor: '#447CCE',
     title: 'Linux Workshop',
     day: '22',
     month: 'Nov',
@@ -60,6 +66,8 @@ const upcomingEvent = [
     filterSetting: 'TechTalks'
   },
   {
+    primaryColor: 'var(--color-button-primary)',
+    secondaryColor: 'var(--color-button-secondary)',
     title: 'Linux Tech talk',
     day: '22',
     month: 'Nov',
@@ -70,6 +78,8 @@ const upcomingEvent = [
     filterSetting: 'TechTalks'
   },
   {
+    primaryColor: 'var(--color-image-background)',
+    secondaryColor: '#ADBED8',
     title: 'Linux Tech talk',
     day: '22',
     month: 'Nov',
