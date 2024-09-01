012f3a587881e23810d132965513f09cef102a37
Michael Dresler <michaeldresler@hotmail.com>
2024-09-01 13:46:23 -0700

    Created playground page to test components

---
+++ src/app/Playground/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Landing from "../pages/Landing";
+
+export default function Playground() {
+  return (
+    <main>
+      <h1 className="large-title">SFU Surge is definitely a club</h1>
+      <p className="">fr fr</p>
+      <a className="button-link md primary">Click here</a>
+    </main>
+  );
+}
