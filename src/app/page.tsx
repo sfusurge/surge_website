4b921bfa887f842da1e2686875356dda40768dd3
Christina <christinaraganit@gmail.com>
2024-09-01 00:37:38 -0700

    initialized variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
+      <h1 className="large-title">SFU Surge is definitely a club</h1>
+      <p className="">fr fr</p>
+      <a className="button-link md primary">Click here</a>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "sunken": "var(--sunken)",
+        "text-primary": "var(--text-primary)",
+        "text-secondary": "var(--text-secondary)",
+        "brand-primary": "var(--brand-primary)",
+        "brand-primary-hover": "var(--brand-primary-hover)",
+        "brand-primary-pressed": "var(--brand-primary-pressed)",      }
     },
   },
   plugins: [],
