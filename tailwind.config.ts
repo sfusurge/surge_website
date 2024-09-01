b8bbd9050401709eff87b5fb1b0e9102d852f9a3
Michael Dresler <michaeldresler@hotmail.com>
2024-09-01 12:46:32 -0700

    added custom font sizes to tailwind config
    Works for now but should revise naming convention and sizes for mobile at a later time

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,20 @@ const config: Config = {
         "text-secondary": "var(--text-secondary)",
         "brand-primary": "var(--brand-primary)",
         "brand-primary-hover": "var(--brand-primary-hover)",
-        "brand-primary-pressed": "var(--brand-primary-pressed)",      }
+        "brand-primary-pressed": "var(--brand-primary-pressed)",      },
+        
+        fontSize:{
+          xs:['0.75rem',{lineHeight:"100%", letterSpacing: "0rem"}],
+          s:['0.875rem',{lineHeight:"100%", letterSpacing: "0rem"}],
+          base:['1.125rem',{lineHeight:"150%", letterSpacing: "0rem"}],
+          lg:['1.25rem',{lineHeight:"100%", letterSpacing: "0rem"}],
+          xl:['1.5625rem',{lineHeight:"125%", letterSpacing: "0rem"}],
+          "2xl":['2rem',{lineHeight:"125%", letterSpacing: "0rem"}],
+          "3xl":['2.5rem',{lineHeight:"125%", letterSpacing: "0rem"}],
+          "4xl":['3.25rem',{lineHeight:"112.5%", letterSpacing: "0rem"}],
+
+
+        }
     },
     fontFamily:{
       GeistSans:['var(--font-geist-sans)'],
