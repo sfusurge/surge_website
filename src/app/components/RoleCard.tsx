bb98dc541ddb0a75ba7c7b2e0f35ffb558a3f4af
Michael Dresler <michaeldresler@hotmail.com>
2024-09-01 17:11:28 -0700

    added hover effect to role card

--- src/app/components/RoleCard.tsx
+++ src/app/components/RoleCard.tsx
@@ -11,7 +11,7 @@ type RoleCardProps = {
     className,
   }: RoleCardProps) {
     return (
-      <div className={`${className} w-full bg-[#121318] rounded-lg flex flex-col p-8 gap-4 `}>
+      <div className={`${className} w-full bg-[#121318] rounded-3xl flex flex-col p-8 gap-4 border border-gray-500/0 hover:border-gray-200/10 transition-all duration-300 cursor-pointer`}>
         <div className="text-sm font-GeistMono uppercase text-[#60A5FA] font-semibold">{team}</div>
         <h2 className="text-xl font-medium">{title}</h2>
         <p className="text-base text-text-secondary font-normal ">{description}</p>
