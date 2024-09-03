4157c3912e1531b9217ac3c3e0241a4f28882631
Michael Dresler <michaeldresler@hotmail.com>
2024-09-02 19:21:27 -0700

    added hero section
    class attached to main should be added to the styling branch at some point as a default way to have consistent margins.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,18 @@
 export default function Page() {
-  return <main></main>;
+  return <main className="mx-6 md:mx-12 lg:mx-auto lg:px-12 flex flex-col justify-center max-w-[90rem]">
+      <section className="max-w-[90rem]  flex justify-center ">
+        <div className="flex flex-col items-center max-w-[38rem] text-center">
+          <div className="mb-5">About Surge</div>
+          <h2 className="mb-6 text-5xl">
+            SFU Surge is the place for you if you’re blah blah
+          </h2>
+          <p className=" text-xl ">
+            The organizers of Simon Fraser University’s largest hackathon, SFU
+            Surge provides an inclusive space for students to explore their
+            passions, connect with industry professionals.
+          </p>
+        </div>
+      </section>
+
+  </main>;
 }
