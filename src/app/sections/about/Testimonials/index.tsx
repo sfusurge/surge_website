14119d0e68ed9f06f5ef2367e892ca2cfd34dba4
ChrisFong604 <ccffoonngg@gmail.com>
2024-09-08 15:06:40 -0700

    moved files into app router to fix styling issues

---
+++ src/app/sections/about/Testimonials/index.tsx
@@ -0,0 +1,9 @@
+export default function Testimonials() {
+  return (
+    <>
+      <section>
+        <h2 className="font-GeistMono text-8xl">This is testimonies</h2>
+      </section>
+    </>
+  );
+}
