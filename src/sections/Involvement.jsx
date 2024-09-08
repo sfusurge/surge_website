c3299134aed4b0d818d830f237e970eb5056732e
ChristopherFong604 <76576876+ChrisFong604@users.noreply.github.com>
2024-09-08 15:20:29 -0700

    62 refactor text (#63)
    ## Describe your changes
    Moved text content from all existing sections to a JSON file in the
    public folder
    
    ## Testing steps 
    1. Checkout to `62-refactor-text`
    2. Run the application
    3. Ensure that the webpage styling and responsiveness is not affected
    4. Try changing the content in the `public/content/text_content.json`
    file, and ensure changes persist on your local development server
    
    ## Issue ticket number and link
    https://github.com/sfusurge/surge_website/issues/62
    
    ## Checklist before requesting a review
    - [x] I have performed a self-review of my code
    - [x] If it is a core feature, I have added thorough tests.
    - [x] Do we need to implement analytics?

--- src/sections/Involvement.jsx
+++ src/sections/Involvement.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React from "react";
 import InvolvementImage from "/public/involvement.png";
-import Image from 'next/image';
+import Image from "next/image";
+
+import text_content from "/public/content/text_content.json";
 
 const Involvement = () => {
   return (
@@ -8,17 +10,17 @@ const Involvement = () => {
       <div className="w-full md:w-[594px] md:h-[264px] flex flex-col justify-center items-center text-center mb-16">
         <div className="text-center mb-16">
           <h2 className="text-brand-primary uppercase tracking-wider text-s mb-4">
-            Get Involved
+            {text_content.homepage.involvement.subtitle}
           </h2>
           <h1 className="text-3xl mb-6">
-              Are you a student at SFU hoping to build connections & host events for the tech community?
+            {text_content.homepage.involvement.title}
           </h1>
           <button className="bg-brand-primary hover:bg-brand-primary-hover text-primary py-2 px-6 rounded-custom-radius transition">
-              <a
-                  href="#opportunities" // TODO: Replace with the actual link URL  
-              >
-                  View opportunities 
-              </a>
+            <a
+              href="#opportunities" // TODO: Replace with the actual link URL
+            >
+              {text_content.homepage.involvement.button_text}
+            </a>
           </button>
         </div>
       </div>
@@ -26,24 +28,21 @@ const Involvement = () => {
         <div className="flex flex-col md:flex-row items-start custom-gap">
           <div className="flex-1">
             <h2 className="uppercase tracking-wider text-brand-primary text-s mb-4">
-              Stay in the Loop
+              {text_content.homepage.involvement.card.subtitle}
             </h2>
             <h1 className="text-3xl mb-4">
-              Be the first to find out about exciting news, like when our next
-              events are!
+              {text_content.homepage.involvement.card.title}
             </h1>
             <p className="text-gray-400 mb-8">
-              The organizers of Simon Fraser University's largest hackathon, SFU
-              Surge provides an inclusive space for students to explore their
-              passions, connect with industry professionals.
+              {text_content.homepage.involvement.card.paragraph}
             </p>
             <button className="button-link bg-brand-primary hover:bg-brand-primary-hover text-primary py-2 px-6 rounded-custom-radius transition">
-                <a
-                    href="#discord" // TODO: Replace with the actual link URL  
-                    // mobile layout in figma shows "Read our lore" - TODO: update to correct text
-                >
-                    Join our Discord server
-                </a>
+              <a
+                href="#discord" // TODO: Replace with the actual link URL
+                // mobile layout in figma shows "Read our lore" - TODO: update to correct text
+              >
+                {text_content.homepage.involvement.card.button_text}
+              </a>
             </button>
           </div>
 
--- src/sections/Landing.jsx
+++ src/sections/Landing.jsx
@@ -1,23 +1,22 @@
-import Background from "/public/assets/landing.png"
+import Background from "/public/assets/landing.png";
 import BodyImage from "/public/assets/landing-2.png";
 import Image from "next/image";
 
-// gray box 
+import text_content from "/public/content/text_content.json";
+// gray box
 // meet the team branch has blue color
 function Landing() {
   return (
     <div>
       <div className="flex flex-col items-center justify-center">
         <button className="bg-surface text-text-primary px-4 py-2 rounded-custom-radius hover:bg-brand-primary-hover">
-          Apply to StormHacks x Enactus now! &rarr;
+          {text_content.homepage.landing.button_text}
         </button>
         <h1 className="mt-5 text-3xl max-w-lg text-center text-text-primary">
-          SFU Surge lorem ipsum dolor sit amet consectetur
+          {text_content.homepage.landing.title}
         </h1>
         <p className="mt-5 text-center max-w-lg text-text-secondary">
-          The organizers of Simon Fraser University's largest hackathon, SFU
-          Surge provides an inclusive space for students to explore their
-          passions, connect with industry professionals.
+          {text_content.homepage.landing.subtitle}
         </p>
         <div className="relative w-full max-w-4xl h-[25rem]">
           <Image
@@ -30,19 +29,22 @@ function Landing() {
         </div>
       </div>
 
-      <div className="flex flex-col-reverse md:flex-row pb-5 mb-5 md:mt-20 justify-between px-5 md:px-10 md:py-8 rounded-custom-radius items-center" style={{ backgroundColor: "rgba(18, 19, 24, 1)" }}>
+      <div
+        className="flex flex-col-reverse md:flex-row pb-5 mb-5 md:mt-20 justify-between px-5 md:px-10 md:py-8 rounded-custom-radius items-center"
+        style={{ backgroundColor: "rgba(18, 19, 24, 1)" }}
+      >
         <div className="">
-          <h2 className="text-brand-primary">WHAT'S SFU SURGE</h2>
+          <h2 className="text-brand-primary">
+            {text_content.homepage.landing.card.subtitle}
+          </h2>
           <h1 className="text-text-primary mt-2 text-2xl max-w-lg">
-            Deadass all we do is play Stardew Valley and League of Legends
+            {text_content.homepage.landing.card.title}
           </h1>
           <p className="mt-3 text-text-secondary max-w-lg">
-            The organizers of Simon Fraser University's largest hackathon, SFU
-            Surge provides an inclusive space for students to explore their
-            passions, connect with industry professionals.
+            {text_content.homepage.landing.card.paragraph}
           </p>
           <button className="mt-5 bg-brand-primary text-text-primary px-4 py-2 rounded-custom-radius hover:bg-brand-primary-hover">
-            Read our lore
+            {text_content.homepage.landing.card.button_text}
           </button>
         </div>
         <div className="relative w-full max-w-2xl h-80 mb-12">
