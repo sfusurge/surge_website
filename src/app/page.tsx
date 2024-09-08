b9b46903a8c4f4730a86bfe982babaf2c7707018
jennaolee <105249401+jennaolee@users.noreply.github.com>
2024-09-07 22:52:39 -0700

    Add Involvement Section (#59)
    ### Describe your changes
    Implemented the Involvement section for the landing page.
    
    ### Issue ticket number and link
    https://github.com/sfusurge/surge_website/issues/58
    
    ### Testing
    1. checkout to 58-involvement
    2. run 'pnpm install'
    3. run 'npm run dev'
    
    ---------
    
    Co-authored-by: Jenna Lee <jenna_lee_3@sfu.ca>
    Co-authored-by: ChrisFong604 <ccffoonngg@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import Image from "next/image";
 import Landing from "../sections/Landing"
 import Events from "../sections/Events"
+import Involvement from "../sections/Involvement"
 
 export default function Home() {
   return (
     <main>
       <Landing />
       <Events />
+      <Involvement />
     </main>
   );
 }
