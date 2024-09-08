83ea476c17d8bdd8b750b33bfb73e8648ad44cf9
ChrisFong604 <ccffoonngg@gmail.com>
2024-09-08 14:42:03 -0700

    Organized sections into respective folders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Landing from "../sections/home/Landing";
-import Events from "../sections/home/Events";
-import Involvement from "../sections/home/Involvement";
+import Events from "../sections/home/Events/index";
+import Involvement from "../sections/home/Involvement/index";
 
 export default function Home() {
   return (
