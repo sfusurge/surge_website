a0c147e70ec9d63539e7c53b9b46d5a59a31a407
ChrisFong604 <ccffoonngg@gmail.com>
2024-09-08 15:35:34 -0700

    remove text in testimonials

--- src/app/sections/about/Testimonials/index.tsx
+++ src/app/sections/about/Testimonials/index.tsx
@@ -1,9 +1,3 @@
 export default function Testimonials() {
-  return (
-    <>
-      <section>
-        <h2 className="font-GeistMono text-8xl">This is testimonies</h2>
-      </section>
-    </>
-  );
+  return <></>;
 }
