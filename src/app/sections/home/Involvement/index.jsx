c1ad0ee9b02380e875dbd254b1025d5399b713b6
ChrisFong604 <ccffoonngg@gmail.com>
2024-09-08 15:54:00 -0700

    updated margins on footer and fixed height in involvement card to prevent text overflow

--- src/app/sections/home/Involvement/index.jsx
+++ src/app/sections/home/Involvement/index.jsx
@@ -6,7 +6,7 @@ import text_content from "/public/content/text_content.json";
 
 const Involvement = () => {
   return (
-    <div className="min-h-screen text-primary flex flex-col items-center py-16 px-6">
+    <div className="min-h-screen text-primary flex flex-col items-center pt-16 px-6">
       <div className="w-full md:w-[594px] md:h-[264px] flex flex-col justify-center items-center text-center mb-16">
         <div className="text-center mb-16">
           <h2 className="text-brand-primary uppercase tracking-wider text-s mb-4">
@@ -24,7 +24,7 @@ const Involvement = () => {
           </button>
         </div>
       </div>
-      <div className="w-full md:w-[1240px] md:h-[469px] bg-surface rounded-custom-radius p-8 w-full flex sm:flex-row md:flex-row lg:flex-col items-start">
+      <div className="md:max-w-[1240px] bg-surface rounded-custom-radius p-8 w-full flex sm:flex-row md:flex-row lg:flex-col items-start">
         <div className="flex flex-col md:flex-row items-start custom-gap">
           <div className="flex-1">
             <h2 className="uppercase tracking-wider text-brand-primary text-s mb-4">
