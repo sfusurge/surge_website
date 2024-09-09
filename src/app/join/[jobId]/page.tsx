c193b444dbb2c3a08aca353bf0ef57b7a81917c5
Michael Dresler <michaeldresler@hotmail.com>
2024-09-09 14:15:45 -0700

    component for job error

--- src/app/join/[jobId]/page.tsx
+++ src/app/join/[jobId]/page.tsx
@@ -1,6 +1,7 @@
 import jobs from "@/lib/jobListing.json"; // Assuming your jobs.json file is here
 import { Metadata } from "next";
 import JoinTeam from "@/app/sections/join/[jobId]";
+import JobError from "@/app/sections/join/JobError";
 
 type JobPageProps = {
   params: {
@@ -22,10 +23,7 @@ export default function JobPage({ params: { jobId } }: JobPageProps) {
   const job = jobs.find((role) => role.url === jobId);
 
   if (!job) {
-    return <div className=" flex w-full  h-[50vh]  items-center justify-center flex-col gap-4 text-center ">
-      <h2 className="title-2">This job doesn't seem to exist</h2>
-      <p className="paragraph">Oops! It looks like the job you're looking for either no longer exists or the link you followed may be incorrect. Please check the URL or explore our current job listings.</p>
-      </div>; // Handle missing job
+    return <JobError/>
   }
   return (
     <JoinTeam job={job} />
---
+++ src/app/sections/join/JobError/index.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function JobError() {
+    return <div className=" flex w-full  h-[50vh]  items-center justify-center flex-col gap-4 text-center ">
+      <h2 className="title-2">This job doesn't seem to exist</h2>
+      <p className="paragraph">Oops! It looks like the job you're looking for either no longer exists or the link you followed may be incorrect. Please check the URL or explore our current job listings.</p>
+      </div>; // Handle missing job
+  }
+
