6ee6b8134559d718d751a5a0bc5f9e3d7d3a334f
Kevin Shi <kevins.shi2001@gmail.com>
2024-09-08 22:53:14 -0700

    pulling new development changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Events from "@/components/Events";
+import Sponsors from "@/components/sponsors"
 export default function Home() {
   return (
     <main>
       <Events />
+      <Sponsors />
     </main>
   );
 }
