bf122cd7f565a39bfce98168354e4af89e00085e
ayana <ayanahye2@gmail.com>
2024-09-24 21:35:35 -0700

    initial CMS commit

--- src/app/sections/events/CurrentEvents/index.tsx
+++ src/app/sections/events/CurrentEvents/index.tsx
@@ -1,18 +1,45 @@
+'use client'
 import EventCard from "@/components/EventCard";
-import eventData from "@/lib/eventsData.json"
+import eventData from "@/lib/eventsData.json";
+import {useEffect, useState} from 'react';
 
+// revise the type declarations -- using any might not be best practice
+// people -- socials
 
+// types in the code -- type safety
+// finish CMS and work on other content models
 
 export default function CurrentEvents() {
+
+  const [data, setData] = useState<any>(null);
   
+// clarifying binary data or formatting in client when making request
+
+  const request_data = useEffect(() => {
+    async function fetchdata() {
+      const res = await fetch("");
+      const result = await res.json();
+      setData(result);
+    } 
+    fetchdata()
+  }, [])
+
+  console.log(data);
 
   return (
     <section className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-      {eventData.map((thisEvent, index) =>{
-          return(
-            <EventCard key={index} title={thisEvent.title} time={thisEvent.time} location={thisEvent.location}/>
-          )
-      } )}
+      {data?.items.map((thisEvent: any, index: any) => {
+        const { title, time, location } = thisEvent.fields; 
+
+        return (
+          <EventCard
+            key={index}
+            title={title}       
+            time={time}        
+            location={location} 
+          />
+        );
+      })}
 
     
     </section>
