9e6ecba850835c977e1c657c07bfaca7324d8712
ChrisFong604 <ccffoonngg@gmail.com>
2024-10-16 11:12:13 -0700

    removed unused imports

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,11 +1,6 @@
 import EventsHero from "../sections/events/EventsHero";
 import JoinDiscord from "@/components/JoinDiscord";
 import CurrentEvents from "../sections/events/CurrentEvents";
-import {
-  fetchEventCollection,
-  fetchJobListingCollection,
-  fetchSpace,
-} from "@/lib/content/contentfulConnector";
 
 export default async function Page() {
   return (
