48ed11043f4f8678d31ed2c5417845c7f0390bf8
goldentoaste <rayg2375@gmail.com>
2024-11-18 01:15:48 -0800

    removed debug code

--- src/components/Starbackground/Starbackground.tsx
+++ src/components/Starbackground/Starbackground.tsx
@@ -9,8 +9,6 @@ export default function Starbackground() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const renderer = useRef<StarRenderer | null>(null);
     useEffect(() => {
-        console.log('what the');
-        
         if (canvasRef.current) {
             // only make the renderer once
             renderer.current = new StarRenderer(canvasRef.current);
@@ -24,7 +22,6 @@ export default function Starbackground() {
     return (
         <div className={style.starfieldRoot}>
             <canvas ref={canvasRef} className={style.starfieldCanvas}></canvas>
-            <h1>BELH</h1>
         </div>
     );
 }
